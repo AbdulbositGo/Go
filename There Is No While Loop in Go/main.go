30b3bef47eb3fe4b3ccde69e6c26dd008f9fce22
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-01-21 21:24:51 +0500

    Refactor loop condition in getMaxMessageToSend function

--- There Is No While Loop in Go/main.go
+++ There Is No While Loop in Go/main.go
@@ -3,12 +3,10 @@ package main
 func getMaxMessageToSend(costMultiplier float64, maxCostInPennies int) int {
 	actualCostInPennies := 1.0
 	maxMessageToSend := 0
-	balance := float64(maxCostInPennies)
 
-	for balance >= actualCostInPennies {
-		balance -= actualCostInPennies
-		actualCostInPennies *= costMultiplier
+	for float64(maxCostInPennies) <= actualCostInPennies {
 		maxMessageToSend++
+		actualCostInPennies *= costMultiplier
 	}
 
 	return maxMessageToSend
