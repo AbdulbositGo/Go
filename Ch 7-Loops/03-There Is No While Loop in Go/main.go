c333473a4fa3d239cb8d095c2e61cbc604b9455c
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-01-30 11:25:12 +0500

    Fizbuzz

--- Ch 7-Loops/03-There Is No While Loop in Go/main.go
+++ Ch 7-Loops/03-There Is No While Loop in Go/main.go
@@ -3,11 +3,9 @@ package main
 func getMaxMessageToSend(costMultiplier float64, maxCostInPennies int) int {
 	actualCostInPennies := 1.0
 	maxMessageToSend := 0
-
 	for float64(maxCostInPennies) <= actualCostInPennies {
 		maxMessageToSend++
-		actualCostInPennies *= costMultiplier
+		actualCostInPennies*=costMultiplier
 	}
-
 	return maxMessageToSend
 }
